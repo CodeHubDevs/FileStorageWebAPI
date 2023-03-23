188624b0e8c3030715b815988e8983a98a0bd1e5
Rommel De Torres <rommel.detorres@triseed.com.ph>
2023-03-23 21:53:25 +0800

    added_file_methods

--- apps/base/api.py
+++ apps/base/api.py
@@ -3,6 +3,7 @@ from apps.authentication.api import router as authentication_router
 from apps.users.api import router as users_router
 from apps.folders.api import router as folder_router
 from apps.modules.api import router as module_router
+from apps.files.api import router as file_router
 from ninja.security import HttpBearer
 
 class GlobalAuth(HttpBearer):
@@ -15,3 +16,4 @@ api.add_router('/', authentication_router)
 api.add_router('/users', users_router)
 api.add_router('/folder', folder_router)
 api.add_router('/modules', module_router)
+api.add_router('/files', file_router)
